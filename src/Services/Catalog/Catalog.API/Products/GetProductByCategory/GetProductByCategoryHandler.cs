232c8d435e1091c09d3f59fedb446f7debaa33e2
LbStoyanov <lbstoyanov1@gmail.com>
2024-10-14 06:24:56 +0300

    Develop GetProductByCategory GET Endpoint with CQRS and MediatR

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategryResult>;
+
+public record GetProductByCategryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler (IDocumentSession session, ILogger<GetProductByCategoryQuery> logger )
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategryResult>
+{
+    public async Task<GetProductByCategryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync();
+
+        return new GetProductByCategryResult(products);
+    }
+}
+
