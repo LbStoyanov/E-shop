2c81b489908fe1022f50e603a78e1df0a7d6e8b1
LbStoyanov <lbstoyanov1@gmail.com>
2024-10-15 20:05:37 +0300

    Develop DeleteProduct Handler with CQRS and MediatR

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProdcutHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProdcutCommandHandler(IDocumentSession session, ILogger<DeleteProdcutCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProdcutCommandHandler.Handle called with {@command}", command);
+
+        session.Delete<Product>(command.Id);
+
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
+
