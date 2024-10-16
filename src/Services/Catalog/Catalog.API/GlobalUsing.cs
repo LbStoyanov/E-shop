60a2b8cc3c0694d4638687073835c8c51f826f9e
LbStoyanov <lbstoyanov1@gmail.com>
2024-10-16 06:44:13 +0300

    Add manual validation with AbstractValidator using Fluent Validation

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -4,5 +4,5 @@ global using MediatR;
 global using Marten;
 global using BuildingBlocks.CQRS;
 global using Catalog.API.Models;
-global using Catalog.API.Products.CreateProduct;
 global using Catalog.API.Exceptions;
+global using FluentValidation;
