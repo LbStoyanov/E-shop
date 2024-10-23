e4c3bfe099c40dba7efc1bfe6fda433ec5b26cb5
LbStoyanov <lbstoyanov1@gmail.com>
2024-10-23 20:15:09 +0300

    BuildingBlocks Handling Exceptions Generic Way with app.UseExceptionHandler

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.API.Products.GetProductById;
 
-public record GetProductByIdQuery(Guid id): IQuery<GetProductByIdResult>;
+public record GetProductByIdQuery(Guid Id): IQuery<GetProductByIdResult>;
 
 public record GetProductByIdResult(Product Product);
 
@@ -12,11 +12,11 @@ internal class GetProductByIdQueryHandler
     {
         logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
 
-        var product = await session.LoadAsync<Product>(query.id,cancellationToken);
+        var product = await session.LoadAsync<Product>(query.Id,cancellationToken);
 
         if (product == null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(query.Id);
         }
 
         return new GetProductByIdResult(product);
