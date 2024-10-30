8f9c607d4f1f4d3f0fdd2fca282e9bf387e0b403
LbStoyanov <lbstoyanov1@gmail.com>
2024-10-30 20:15:59 +0200

    Register Custom Exception Handlin into ASP.NET with AddExceptionHandler method

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,4 @@
-using BuildingBlocks.Behaviors;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,43 +17,17 @@ builder.Services.AddCarter();
 
 builder.Services.AddMarten(options =>
 {
-
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
-
-
 }).UseLightweightSessions(); //Used for perfrmance
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 
 var app = builder.Build();
 
 app.MapCarter();
 
-//Global Exception Handling Mechanism
-app.UseExceptionHandler(exceptionHandlerApp =>
-{
-    exceptionHandlerApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+app.UseExceptionHandler(option => { });
 
-        if (exception == null)
-            return;
-
-        var problemDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace,
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
-});
 
 app.Run();
