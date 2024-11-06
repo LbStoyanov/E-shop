5fd002a8c364944d5ee85b3066bb978408208a22
LbStoyanov <lbstoyanov1@gmail.com>
2024-11-06 06:37:07 +0200

    Implement Pagination logic for GetProducts Query with ToPagedListAsync extention method included in Marten

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,17 +1,17 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
 
-public record GetProductsQuery() : IQuery<GetProdustsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProdustsResult>;
 
 public record GetProdustsResult(IEnumerable<Product> Products);
 
-internal class GetProductsQueryHandler (IDocumentSession session)
+internal class GetProductsQueryHandler(IDocumentSession session)
     : IQueryHandler<GetProductsQuery, GetProdustsResult>
 {
     public async Task<GetProdustsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
 
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
         return new GetProdustsResult(products);
     }
