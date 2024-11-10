bddd4270173465a5b574a19d4ba1f81444d263b6
LbStoyanov <lbstoyanov1@gmail.com>
2024-11-10 21:44:53 +0200

    Develop Basket.API Microservice Creating Domain Entities ShoppingCart and ShoppingCartItem

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.API.Models;
+public class ShoppingCart
+{
+    //Required for mapping
+    public ShoppingCart()
+    {
+        
+    }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+
+    public string UserName { get; set; } = default!;
+
+    public List<ShoppingCartItem> Items { get; set; } = new();
+
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
+
