5093da810d819088dfa7e3e9e3e76ab2c44fd998
LbStoyanov <lbstoyanov1@gmail.com>
2024-11-21 12:11:55 +0200

    Register Custom Exception Handling for Basket API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -21,10 +21,12 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions(); // For better performance
 
 builder.Services.AddScoped<IBasketRepository,BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 //Confugure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
