79c6716839b6dcebadd0831369340137cf73bff2
LbStoyanov <lbstoyanov1@gmail.com>
2024-12-02 18:30:59 +0200

    Develop DiscountService class to Implement gRPC Proto Service Methods

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,26 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+
+public class DiscountContext : DbContext
+{
+
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = " Galaxy S24 Ultra", Description = "Some product description", Amount = 2000 },
+            new Coupon { Id = 2, ProductName = " Galaxy S22 Ultra", Description = "Some product description", Amount = 1000 }
+        );
+    }
+
+}
+
