1b1e5d6f077370be6a2d57e7f85a2974dd2f2231
LbStoyanov <lbstoyanov1@gmail.com>
2024-12-12 09:24:28 +0200

    Create DDD Abstractions(Entity,Aggregate,DomainEvenet) in Ordering.Domain Layer

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Ordering.Domain.Abstractions;
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    public IReadOnlyList<IDomainEvent> DomainEvents => throw new NotImplementedException();
+
+    public IDomainEvent[] ClearDdomainEvents()
+    {
+        throw new NotImplementedException();
+    }
+}
+
